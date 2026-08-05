91829aa5e6e0b22afdd6e017d6770204c32108e3
Ivan Sumbulov <machalka245@gmail.com>
2026-08-05 10:12:41 +0300

    Fix remark

--- docker-entrypoint-admin.sh
+++ docker-entrypoint-admin.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
-cd tg_bot_calendar/ &&
-python manage.py migrate --no-input &&
-python manage.py runserver 0.0.0.0:8000
+set -e
+
+cd tg_bot_calendar/
+
+python manage.py migrate --no-input
+python manage.py collectstatic --no-input
+
+exec gunicorn tg_bot_calendar.wsgi:application --bind 0.0.0.0:8000 --workers "${GUNICORN_WORKERS:-3}"
--- tg_bot_calendar/tg_bot_calendar/settings.py
+++ tg_bot_calendar/tg_bot_calendar/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -129,3 +130,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+WHITENOISE_AUTOREFRESH = DEBUG
+WHITENOISE_USE_FINDERS = DEBUG
